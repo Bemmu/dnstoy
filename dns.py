ee2a446d7e8f6e7a22e80e7b1461b58324d49004
Bemmu Sepponen <me@bemmu.com>
2017-09-29 15:29:35 +0900

    Clean up the code a bit

--- dns.py
+++ dns.py
@@ -9,9 +9,30 @@ import struct
 import pprint
 import socket
 
+DNS_PORT = 53
+RCODES = {
+	0: "No error condition.",
+	1: "Format error - The name server was unable to interpret the query.",
+	2: "Server failure - The name server was unable to process this query due to a problem with the name server.",
+	3: "Name Error - Meaningful only for responses from an authoritative name server, this code signifies that the domain name referenced in the query does not exist.",
+	4: "Not Implemented - The name server does not support the requested kind of query.",
+	5: "Refused - The name server refuses to perform the specified operation for policy reasons.  For example, a name server may not wish to provide the information to the particular requester, or a name server may not wish to perform a particular operation (e.g., zone transfer) for particular data."
+}
+QTYPES = {
+	0x0001: "A",
+	0x0002: "NS",
+	0x0005: "CNAME",
+	0x0006: "SOA",
+	0x000B: "WKS",
+	0x000C: "PTR",
+	0x000F: "MX",
+	0x0021: "SRV",
+	0x001C: "AAAA"
+}
+QTYPESi = dict((v, k) for k, v in QTYPES.items())
+
 # First 16 bits is an ID for the query, so that responses can be matched
-#ID = random.randint(0, 65535) # 16
-ID = 2
+ID = random.randint(0, 65535) # 16
 print "Picked ID", ID
 
 # Second 16 bits 
@@ -43,22 +64,6 @@ def pack_bits(field_definitions):
 
 header = struct.pack('!HHHHHH', ID, pack_bits(second_fields), QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT)
 
-# header = struct.pack('!H', 0) # shortcut since all were zeroes
-# print " ".join(hex(ord(c)) for c in header)
-
-QTYPES = {
-	0x0001: "A",
-	0x0002: "NS",
-	0x0005: "CNAME",
-	0x0006: "SOA",
-	0x000B: "WKS",
-	0x000C: "PTR",
-	0x000F: "MX",
-	0x0021: "SRV",
-	0x001C: "AAAA"
-}
-QTYPESi = dict((v, k) for k, v in QTYPES.items())
-
 query = "google.com."
 qname = "".join([struct.pack('!B', len(label)) + label for label in query.split('.')])
 QTYPE = QTYPESi['A'] # 1 means querying for A record (CNAME is 5, MX is 15)
@@ -69,43 +74,21 @@ qclass = struct.pack('!H', QCLASS)
 question = qname + qtype + qclass
 
 data = header + question
-# Seemed to be valid, matching data in the screenshot at 
-# https://superuser.com/questions/523917/dns-queries-returning-no-answer-section
-
-# print " ".join(hex(ord(c)) for c in data)
 
 # Exclamation mark means unpack in network order, H means unsigned short, B is byte
 message_id, a, b = struct.unpack('!HBB', data[0:4]) 
-# print "(%s %s)" % (bin(a), bin(b))
-
-# qname, qtype, qclass
 
 # Send the data
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-DNS_PORT = 53
 dest = ('8.8.8.8', DNS_PORT)
 s.sendto(data, dest)
-s_ip, s_port = s.getsockname()
 
+s_ip, s_port = s.getsockname()
 print "Sending DNS packet via UDP %s:%s -> %s:%s" % (s_ip, s_port, dest[0], dest[1])
 
-# Listen for response?
-# Where does the server send the answer to?
-# Does it know the source port and send it there?
-
-# s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# s2.bind(('0.0.0.0', s_port))
 response, addr = s.recvfrom(1024)
 print "Received DNS packet!"
 
-RCODES = {
-	0: "No error condition.",
-	1: "Format error - The name server was unable to interpret the query.",
-	2: "Server failure - The name server was unable to process this query due to a problem with the name server.",
-	3: "Name Error - Meaningful only for responses from an authoritative name server, this code signifies that the domain name referenced in the query does not exist.",
-	4: "Not Implemented - The name server does not support the requested kind of query.",
-	5: "Refused - The name server refuses to perform the specified operation for policy reasons.  For example, a name server may not wish to provide the information to the particular requester, or a name server may not wish to perform a particular operation (e.g., zone transfer) for particular data."
-}
 for x in range(6, 16): 
 	RCODES[x] = "Reserved for future use."
 
@@ -150,21 +133,6 @@ def parse_question_section(section):
 	print "Parsing question section"
 
 	bytes_read, domain_name = parse_label(section)
-
-	# # Length of string, followed by actual string bytes for each part of domain name ("label").
-	# # Zero length label means end of domain name.
-	# i = 0
-	# parts = []
-	# while True:
-	# 	part_length = ord(section[i])
-	# 	if part_length == 0:
-	# 		break
-	# 	parts.append(section[i+1:i+1+part_length])
-	# 	i += part_length + 1
-
-	# domain_name = ".".join(parts)
-
-	# i += 1 # Skip the byte indicating zero length 
 	qtype, qclass = struct.unpack('!HH', section[bytes_read:bytes_read+4])
 
 	print "Received answer for", domain_name
@@ -195,6 +163,8 @@ def parse_answer_section(section, whole_response):
 		print "Not a pointer... not implemented"
 		exit()
 
+	# Then comes the TYPE represented as two bytes which is the requested type (A, MX etc.)
+	# And again "Internet" as 0x00 0x01
 	qtype = struct.unpack('!H', section[name_length:name_length + 2])[0]
 	print "Answer section QTYPE:", QTYPES[qtype]
 	record_class = struct.unpack('!H', section[name_length + 2:name_length + 2 + 2])[0]
@@ -207,9 +177,8 @@ def parse_answer_section(section, whole_response):
 	rdlength = struct.unpack('!H', section[name_length + 2 + 2 + 4:name_length + 2 + 2 + 4 + 2])[0]
 	print "RDLENGTH:", rdlength, "(RDATA section length)"
 
-	# Now next comes the IP address!
-
 	if QTYPES[qtype] == "A":
+		# Now next comes the actual IP address
 		lip = struct.unpack('!BBBB', section[name_length + 2 + 2 + 4 + 2:name_length + 2 + 2 + 4 + 2 + 4])
 		ip_address = ".".join([str(b) for b in lip])
 		print "\n"
@@ -218,11 +187,6 @@ def parse_answer_section(section, whole_response):
 	else:
 		print "Answer section parsing for this qtype not implemented"
 		exit()
-
-	# Then comes the TYPE represented as two bytes which is the requested type (A, MX etc.)
-	# And again "Internet" as 0x00 0x01
-
-	# label_length, domain_name = parse_label(section)
 
 def parse_response(response):
 	id_bytes = 2
@@ -249,7 +213,5 @@ def parse_response(response):
 	parse_answer_section(answer_section, response)
 
 response_hexed = " ".join(hex(ord(c)) for c in response)
-# print "Received message len %s from %s: %s" % (len(response), addr, response_hexed)
-# print ID
 print "Parsing response"
 parse_response(response)

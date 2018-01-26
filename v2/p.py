e4d26b230474d2a3c86ab0d2ddc99c67d2653673
Bemmu Sepponen <me@bemmu.com>
2018-01-26 13:00:32 +0900

    Fix bug with retries. Add more tracked stats to status line.

--- v2/p.py
+++ v2/p.py
@@ -1,9 +1,11 @@
-MAX_CONCURRENT = 1000
-REASK_IN_SECONDS = 5.0
+MAX_CONCURRENT = 5000
+REASK_IN_SECONDS = 1.0
 
 # 16.01.2018: 21 seconds to resolve 50 domains at 50 parallel
 # 17.01.2018: 8 seconds
-# 18.01.2018: 7 seconds to resolve 500 domains at 500 parallel
+# 18.01.2018: 7 seconds to resolve 500 domains at 500 parallel (71 per second)
+# 24.01.2018: 33 seconds to resolve 5000 domains at 5000 parallel (151 per second)
+# 25.01.2018: 0.5% of domains could not be resolved, seems acceptable and could do second pass on those
 
 import random
 import time
@@ -16,10 +18,15 @@ import sys
 import pickle
 import logging
 import collections
+import inspect
+
+def exit():
+	print "Exiting because asked to: %s" % str(inspect.stack()[1])
+	sys.exit()
 
 query_count = collections.defaultdict(int) 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 A_RECORD_RDTYPE = 1
 NS_RDTYPE = 2
@@ -39,6 +46,13 @@ resolved_domains = {
 	# None means NXDOMAIN
 }
 
+# For printing status info
+last_status_print = time.time()
+messages_sent = 0
+replies_received_count = 0
+bytes_received = 0
+retry_count = 0
+
 root_servers = []
 def resolve_root_servers():
 	for ch in 'abcdefghijklm':
@@ -51,14 +65,15 @@ resolve_root_servers()
 print resolved_domains
 
 logging.info("Reading domain list.")
-domains = [l.split(",")[1].strip() for l in open('../opendns-top-1m.csv')][0:500]
+domains = [l.split(",")[1].strip() for l in open('../opendns-top-1m.csv')]#[0:1000000]
 # domains = ["pages.tmall.com"]
 # domains = ['yandex.ru', 'express.co.uk', 'olx.com.eg', 'dailystar.co.uk', 'e1.ru', 'pku.edu.cn', 'fudan.edu.cn', 'www.gov.cn.qingcdn.com']
 # domains = ['www.gov.cn.qingcdn.com']
 # domains = ['ns0-e.dns.pipex.net']
 # domains = ['ads.gold']
 # domains = ['oxforddictionaries.com']
-domains = ['google.com']
+# domains = ['google.com']
+# domains = ['ahram.org.eg']
 
 tlds = list(set([d.split(".")[-1] for d in domains]))
 
@@ -111,10 +126,10 @@ def print_results(resolved_domains, domains):
 		for domain in domains:
 			f.write("%s, " % domain)
 
-			d = resolved_domains[domain]
+			d = resolved_domains.get(domain, None)
 			while d is not None and not is_ip_address(d):
 				# f.write("%s -> " % d)
-				d = resolved_domains[d]
+				d = resolved_domains.get(d, None)
 
 			if d is None:
 				# f.write("None (NXDOMAIN or no ip)")
@@ -133,7 +148,7 @@ except:
 	with open('zone.pickle', 'w') as f:
 		pickle.dump(tld_nameservers, f)
 
-# Just to make lookups faster, records whether a domain is in domains_that_need_querying or domains_being_queried_latest_last
+# Just to make lookups faster, records whether a domain is in domains_that_need_querying or domains_being_queried_newest_last
 in_todo_or_ongoing = {}
 
 domains_that_need_querying = [(domain, random_name_server_by_tld(domain)) for domain in domains]
@@ -143,8 +158,8 @@ for domain, _ in domains_that_need_querying:
 def add_to_todo(domain, first = True, next_ask = None):
 	if domain in in_todo_or_ongoing:
 		logging.debug("Domain %s was already being queried" % domain)
-		print domains_that_need_querying
-		print domains_being_queried_latest_last
+		# print domains_that_need_querying
+		# print domains_being_queried_newest_last
 	else:
 		if not next_ask:
 			next_ask = random_name_server_by_tld(domain)
@@ -163,7 +178,7 @@ def add_to_todo(domain, first = True, next_ask = None):
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setblocking(False)
 
-domains_being_queried_latest_last = [
+domains_being_queried_newest_last = [
 	# ('google.com', 5), ('example.com', 100)...
 ]
 
@@ -179,25 +194,29 @@ def send_async_dns_query(domain, name_server_ip_address):
 def is_ip_address(str):
 	return all([ch in "0123456789." for ch in str])
 
-last_status_print = time.time()
-messages_sent = 0
-replies_received_count = 0
-bytes_received = 0
 
 def send_queries():
 	logging.debug("send_queries()")
 	global messages_sent
 
-	while len(domains_being_queried_latest_last) < MAX_CONCURRENT:
-		if not domains_that_need_querying and domains_being_queried_latest_last:
+	while len(domains_being_queried_newest_last) < MAX_CONCURRENT:
+		if not domains_that_need_querying and domains_being_queried_newest_last:
 			logging.debug("Nothing to do but wait now")
-			time.sleep(0.5)
+			# time.sleep(0.5)
 			return
 
 		try:
-			# print "Do one (%s)" % len(domains_being_queried_latest_last)
+			# print "Do one (%s)" % len(domains_being_queried_newest_last)
 			domain, next_ask = domains_that_need_querying.pop(0)
-			del in_todo_or_ongoing[domain]
+			try:
+				del in_todo_or_ongoing[domain]
+			except KeyError, e: # Not sure how this can happen, but it does.
+				# This is normal, because some packet thought to be expired might have responded later and contained
+				# the answer.
+				continue
+
+				# logging.warning("Tried to remove %s from in_todo_or_ongoing, but it wasn't there!?" % domain)
+				# exit()
 
 			if domain in resolved_domains:
 				logging.debug("Almost asked about %s despite knowing answer %s already" % (domain, resolved_domains[domain]))
@@ -247,7 +266,7 @@ def send_queries():
 					print "Sending packet about %s to %s" % (domain, next_ask)
 				send_async_dns_query(domain, next_ask)
 				messages_sent += 1
-				domains_being_queried_latest_last.append((domain, time.time()))
+				domains_being_queried_newest_last.append((domain, time.time()))
 				in_todo_or_ongoing[domain] = True
 			else:
 				logging.warning("Asked about %s too many times (%s), giving up." % (domain, query_count[domain]))
@@ -255,33 +274,40 @@ def send_queries():
 				resolved_domains[domain] = None
 
 		except IndexError:
-			if len(domains_being_queried_latest_last) == 0:
+			if len(domains_being_queried_newest_last) == 0:
 				print_results(resolved_domains, domains)
 				exit()
 	else:
 		pass
 
+def oldest_elapsed():
+	domain, query_timestamp = domains_being_queried_newest_last[0]
+	return time.time() - query_timestamp
+
+def newest_elapsed():
+	domain, query_timestamp = domains_being_queried_newest_last[-1]
+	return time.time() - query_timestamp
+
 def retry_queries():
+	global retry_count
 	logging.debug("retry_queries()")
 
 	# Retry queries after some time
-	if domains_being_queried_latest_last:
-		while True:
-			domain, query_timestamp = domains_being_queried_latest_last[0]
-			oldest_elapsed = time.time() - query_timestamp
-			if oldest_elapsed > REASK_IN_SECONDS:
-				domains_being_queried_latest_last.pop(0)
+	if domains_being_queried_newest_last:
+		while domains_being_queried_newest_last and oldest_elapsed() > REASK_IN_SECONDS:
+			retry_count += 1
+			domain, timestamp = domains_being_queried_newest_last.pop(0)
+			try:
 				del in_todo_or_ongoing[domain]
-				if "root-servers.net" in domain:
-					print "Foo2"
-					exit()
+			except KeyError, e:
+				logging.warning("Tried to delete %s from lookup dict, but it wasn't there." % domain)
 
-				add_to_todo(domain, first = True)
-				logging.debug("Answer for %s took too long, asking again starting from root." % domain)
-				break
-			else:
-				logging.debug("Nothing expired (oldest %.2f seconds old)" % oldest_elapsed)
-				break
+			if "root-servers.net" in domain:
+				print "Foo2"
+				exit()
+
+			add_to_todo(domain, first = True)
+			logging.debug("Answer for %s took too long, asking again starting from root." % domain)
 
 def recvfrom():
 	return s.recvfrom(1024)
@@ -302,8 +328,8 @@ def receive_next_dns_reply():
 	try:
 		bytes_received += len(data)
 		response = parse(data)
-		log_response(response)
-	except dns.message.TrailingJunk:
+		# log_response(response)
+	except (dns.message.TrailingJunk, dns.exception.FormError): # trailing junk / malformed message
 		logging.warning("Failed to parse response to %s from %s. Trying again starting from random root." % (domain, addr[0]))
 		if "root-servers.net" in domain:
 			print "Foo3"
@@ -330,44 +356,44 @@ def handle_nxdomain(domain):
 def handle_answer(response, domain):
 	global domains_that_need_querying
 
-	if response.answer[0].rdtype == CNAME_RECORD_RDTYPE:
-		cname = str(response.answer[0][0])[:-1].lower()
-		logging.debug("Got CNAME for %s: %s" % (domain, cname))
-		resolved_domains[domain] = cname
+	for answer in response.answer:
+		if answer.rdtype == CNAME_RECORD_RDTYPE:
+			cname = str(answer[0])[:-1].lower()
+			logging.debug("Got CNAME for %s: %s" % (domain, cname))
+			resolved_domains[domain] = cname
 
-		# Now need to resolve the CNAME as well
-		if not cname in resolved_domains:			
-			if "root-servers.net" in cname:
-				print "Foo5"
-				exit()
+			# Now need to resolve the CNAME as well
+			if not cname in resolved_domains:			
+				if "root-servers.net" in cname:
+					print "Foo5"
+					exit()
 
-			add_to_todo(cname, first = True)
+				add_to_todo(cname, first = True)
 
-		# If you encounter detail.tmall.com with CNAME detail.tmall.com.danuoyi.tbcache.com then
-		# you are supposed to connect to the IP of detail.tmall.com.danuoyi.tbcache.com BUT still
-		# say Host: detail.tmall.com
-	elif response.answer[0].rdtype == A_RECORD_RDTYPE:
-		answer_name = str(response.answer[0].name)[:-1].lower()
-		answer_ip = str(response.answer[0][0])
-		resolved_domains[answer_name] = answer_ip
-		logging.debug("The answer is %s: %s" % (answer_name, answer_ip))
-		print resolved_domains
+			# If you encounter detail.tmall.com with CNAME detail.tmall.com.danuoyi.tbcache.com then
+			# you are supposed to connect to the IP of detail.tmall.com.danuoyi.tbcache.com BUT still
+			# say Host: detail.tmall.com
+		elif answer.rdtype == A_RECORD_RDTYPE:
+			answer_name = str(answer.name)[:-1].lower()
+			answer_ip = str(answer[0])
+			resolved_domains[answer_name] = answer_ip
+			logging.debug("The answer is %s: %s" % (answer_name, answer_ip))
+			# print resolved_domains
 
-		# Make sure not to ask about this again if we have it queued.
-		before = len(domains_that_need_querying)
-		domains_that_need_querying = [(d, n) for d,n in domains_that_need_querying if d != answer_name]
-		try:
-			del in_todo_or_ongoing[answer_name]
-		except KeyError:
-			pass # not necessarily there
-		# logging.debug("Domains that need querying: %s" % domains_that_need_querying)
-		after = len(domains_that_need_querying)
-		if before != after:
-			logging.debug("Removed %s from todo list." % answer_name)
+			# Make sure not to ask about this again if we have it queued.
+			before = len(domains_that_need_querying)
+			domains_that_need_querying = [(d, n) for d,n in domains_that_need_querying if d != answer_name]
+			try:
+				del in_todo_or_ongoing[answer_name]
+			except KeyError:
+				pass # not necessarily there
+			# logging.debug("Domains that need querying: %s" % domains_that_need_querying)
+			after = len(domains_that_need_querying)
+			if before != after:
+				logging.debug("Removed %s from todo list." % answer_name)
 
-	else:
-		logging.debug("Answer rdtype for %s was not A but %s" % (domain, response.answer[0].rdtype))
-		exit()
+		else:
+			logging.warning("Ignoring answer rdtype for %s was not A but %s in:\n%s" % (domain, answer.rdtype, response.to_text()))
 
 def get_seconds_elapsed_since_last_picked(server):
 	try:
@@ -397,10 +423,10 @@ def prioritize_servers(authority_names):
 
 def get_authority_names(response):
 	# Authority section has the name servers to ask next.
-	authority_names = [str(auth) for auth in response.authority[0] if auth.rdtype == NS_RDTYPE]
+	authority_names = [str(auth).lower() for auth in response.authority[0] if auth.rdtype == NS_RDTYPE]
 
 	# Remove ones that didn't resolve.
-	authority_names = [a for a in authority_names if not (a in resolved_domains and resolved_domains[a] is None)]
+	authority_names = [a for a in authority_names if not (a[:-1] in resolved_domains and resolved_domains[a[:-1]] is None)]
 
 	return authority_names
 
@@ -423,17 +449,17 @@ def choose_authority(authority_names):
 
 def ask_forward(authority_name, domain):
 	# If this IP was not in additional, then need to resolve it first.
-	logging.debug("%s known?" % authority_name)
+	# logging.debug("%s known?" % authority_name)
 
 	try:
 		next_ask = resolved_domains[authority_name]
-		logging.debug("Yes. Knew %s has ip %s." % (authority_name, next_ask))
+		# logging.debug("Yes. Knew %s has ip %s." % (authority_name, next_ask))
 
 		if not next_ask:
 			print "Was going to put None to next_ask part 1 because authority %s of domain %s was resolved to None!" % (authority_name, domain)
 			exit()
 	except KeyError:
-		logging.debug("No. Resolving %s first." % authority_name)
+		# logging.debug("No. Resolving %s first." % authority_name)
 		if "root-servers.net" in authority_name:
 			print authority_name
 			print "Foo6"
@@ -453,7 +479,7 @@ def ask_forward(authority_name, domain):
 	add_to_todo(domain, first = False, next_ask = next_ask)
 
 	# domains_that_need_querying.append((domain, next_ask))
-	logging.debug("Domains that need querying: %s" % domains_that_need_querying)
+	# logging.debug("Domains that need querying: %s" % domains_that_need_querying)
 	# logging.debug("Will try to resolve %s by asking %s" % domains_that_need_querying[-1])
 	# print domains_that_need_querying
 
@@ -468,8 +494,9 @@ def handle_authority(response, domain):
 		return
 
 	authority_name = choose_authority(authority_names)
-	set_last_pick_timestamp(authority_name)
-	ask_forward(authority_name, domain)
+	if authority_name:
+		set_last_pick_timestamp(authority_name)
+		ask_forward(authority_name, domain)
 
 def handle_response(response, domain):
 	global domains_that_need_querying
@@ -522,11 +549,19 @@ def handle_response(response, domain):
 		# logging.debug("Domains that need querying: %s" % domains_that_need_querying)
 
 
+def remove_from_domains_being_queried(domain):
+	global domains_being_queried_newest_last
+	domains_being_queried_newest_last = [x for x in domains_being_queried_newest_last if x[0] != domain]
+	try:
+		del in_todo_or_ongoing[domain]
+	except:
+		pass
+
 def read_all_from_socket():
 
 	logging.debug("read_all_from_socket()")
 
-	global domains_being_queried_latest_last
+	global domains_being_queried_newest_last
 	global replies_received_count
 
 	# Loop until nothing more to read from socket.
@@ -536,9 +571,16 @@ def read_all_from_socket():
 			if not response: # parsing failed, already caused a retry later
 				continue
 
+			if len(response.question) == 0:
+				logging.warning("Ignoring packet from %s because it did not contain a question section: %s" % (addr[0], response.to_text()))
+				continue
+
+			if len(response.question[0].name) == 0:
+				logging.warning("Ignoring packet from %s because it had zero-sized name section: %s" % (addr[0], response.to_text()))
+				continue
+
 			domain = str(response.question[0].name)[:-1]
-			domains_being_queried_latest_last = [x for x in domains_being_queried_latest_last if x[0] != domain]
-			del in_todo_or_ongoing[domain]
+			remove_from_domains_being_queried(domain)
 
 			replies_received_count += 1
 
@@ -556,23 +598,38 @@ def read_all_from_socket():
 			else:
 				logging.warning("Some other error (%s) on socket" % e.errno)
 
-while True:
-	ongoing_count = len(domains_being_queried_latest_last)
-	todo_count = len(domains_that_need_querying)
-
+def print_status():
+	global last_status_print
 	elapsed_since_last_status_print = time.time() - last_status_print
 	if elapsed_since_last_status_print > 1.0:
-		logging.info("%s queries ongoing, %s to do, %s sent, %s responses / %s kB" % (ongoing_count, todo_count, messages_sent, replies_received_count, bytes_received/1024))
+		status = "%s ongoing, %s retry, %s to do, %s sent, %s responses / %s kB"
+		fields = (ongoing_count, retry_count, todo_count, messages_sent, replies_received_count, bytes_received/1024)
+		if domains_being_queried_newest_last:
+			status += ", oldest %.2fs, newest %.2fs"
+			fields += (oldest_elapsed(), newest_elapsed()) 
+		logging.info(status % fields)
+
 		last_status_print = time.time()
 
 		if ongoing_count < 10:
-			logging.info('ongoing: %s' % [x[0] for x in domains_being_queried_latest_last])
+			logging.info('ongoing: %s' % [x[0] for x in domains_being_queried_newest_last])
 		if todo_count < 10:
-			logging.info('todo: %s' % [x for x in domains_that_need_querying])
+			logging.info('todo: %s' % [x for x in domains_that_need_querying])	
 
+while True:
+	ongoing_count = len(domains_being_queried_newest_last)
+	todo_count = len(domains_that_need_querying)
+
+	print_status()
 	# time.sleep(0.4)
 
 	sys.stdout.flush()
+
+	print "Checking for multiples"
+	if len(set(domains_that_need_querying)) != len(domains_that_need_querying):
+		print "Domain appeared multiple times!"
+		exit()
+	print "Done check"
 
 	retry_queries()
 	send_queries()
